c1e3e71f0f0eea6e41feeb29876e6623e519a02e
liruijie <lirj@higoauto.com>
2019-01-03 14:13:48 +0800

    delete some code

--- ITC100/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := /usr/lib64/libstdc++.so.6
-
-LIBS := -lpthread -lm -lnsl -lclntsh -locci
-
